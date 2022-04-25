81a7704310c3d02e1e3ed4a4e2819fc02b3277f8
Jacob Miller <jacob.miller1850@gmail.com>
2022-04-25 18:49:36 -0400

    improvement to the ref trajectory; fixed a couple bugs

--- src/merge/create_plots.py
+++ src/merge/create_plots.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-merge = np.load('plot_data/flow_10.0.npy')
-leading = np.load('plot_data/flow_00.0.npy')
+merge = np.load('plot_data/flow_10.0.npy', allow_pickle=True)
+leading = np.load('plot_data/flow_00.0.npy', allow_pickle=True)
 
 acc_merge = merge[:,2].astype(np.float)
 acc_leading = leading[:,2].astype(np.float)
 
 pos_merge = merge[:,3].astype(np.float)
 pos_leading = leading[:,3].astype(np.float)
+
+vel_merge = merge[:,4].astype(np.float)
+vel_leading = leading[:,4].astype(np.float)
 
 plt.title("Accelerations")
 plt.xlabel("Time Step")
@@ -23,5 +26,13 @@ plt.xlabel("Time Step")
 plt.ylabel("Positions (m)")
 plt.plot(pos_merge, color ="red", label = "merging vehicle")
 plt.plot(pos_leading, color ="green", label = "leading inflow vehicle")
+plt.legend(loc="upper right")
+plt.show()
+
+plt.title("Velocities")
+plt.xlabel("Time Step")
+plt.ylabel("Velocities (m/s)")
+plt.plot(vel_merge, color ="red", label = "merging vehicle")
+plt.plot(vel_leading, color ="green", label = "leading inflow vehicle")
 plt.legend(loc="upper right")
 plt.show()
